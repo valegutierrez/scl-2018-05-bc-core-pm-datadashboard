24f0c3e3452107884205b9741668639f35dc79ec
Valentina <vale.gutierrezc@gmail.com>
2018-06-20 17:07:45 -0400

    working on computeUserStats

--- src/js/data.js
+++ src/js/data.js
@@ -1,30 +1,42 @@
 const usersJson = 'https://api.laboratoria.la/cohorts/scl-2018-05-bc-core-pm/users';
 const cohortsJson = 'https://api.laboratoria.la/cohorts';
 const progressJson = 'https://api.laboratoria.la/cohorts/scl-2018-05-bc-core-pm/progress';
+var users = '';
+var courses = '';
+var progress = '';
 
 fetch(usersJson)
   .then(response => response.json())
-  .then(dataUsers => {
-    lectureProgress(dataUsers);
-    findUser(dataUsers);
+  .then(usersData => {
+    users = usersData;
+    lectureProgress(usersData);
+    findUser(usersData);
+    generalInformation(usersData);
   });
 
 fetch(cohortsJson)
   .then(response => response.json())
-  .then(dataCohorts => {
-    getCohorts(dataCohorts);
+  .then(cohortsData => {
+    const filterCourses = cohortsData.forEach(element => {
+      courses += element.courseIndex;
+    });
+    console.log(courses);
+    getCohorts(cohortsData);
   });
 
 fetch(progressJson)
   .then(response => response.json())
-  .then(dataCohorts => {
-    console.log(dataCohorts);
+  .then(progressData => {
+    progress = progressData;
+    console.log(progress);
   });
 
-const getCohorts = dataCohorts => {
-  const renderCohorts = dataCohorts.forEach(element => {
-    let cohort = `<a class="dropdown-item" href="#">${element.id}</href>`;
-    return cohortsList.innerHTML += cohort;
-  });
-  return renderCohorts;
-};
+function start() {
+  if (users && progress && courses) {
+    computeUserStats(users, progress, courses);
+  }
+}
+
+function computeUserStats(users, progress, courses) {
+
+}
