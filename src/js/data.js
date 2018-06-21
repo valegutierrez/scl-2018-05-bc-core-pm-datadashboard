c88ac5df60639607a357ef11ade9e6dc60759bf4
MLoretto <loreto.henriquez.m@gmail.com>
2018-06-21 01:49:47 -0400

    Se crea lógica computeUserStat

--- src/js/data.js
+++ src/js/data.js
@@ -1,7 +1,8 @@
 const usersJson = 'https://api.laboratoria.la/cohorts/scl-2018-05-bc-core-pm/users';
 const cohortsJson = 'https://api.laboratoria.la/cohorts';
 const progressJson = 'https://api.laboratoria.la/cohorts/scl-2018-05-bc-core-pm/progress';
-
+//testData
+let Progress = {};
 function start() {
   Promise.all([ // Llama la info de API en paralelo(Todas a la vez)
     fetch('https://api.laboratoria.la/cohorts/scl-2018-05-bc-core-pm/users'),
@@ -31,6 +32,80 @@ function start() {
 }
 function computeUserStats(users, progress, courses) {
   console.log('Entraron los Datos en computeUserStats');
+  console.log(users[0]);
+  Progress = progress['axUVrtJBsWgK8aqCcAh4qN8z69N2'];//users[0].id];
+
+
+  users.forEach(element => {
+    let Progress = progress[element.id];
+    console.log('Alumna:' + element.name);
+// Empieza lógica 
+    let porcentajetotal = 0;
+    for (var Pkey in Progress) {
+      console.log('unidad=' + Pkey);
+      for (var UKey in Progress[Pkey]) {
+        if (UKey === 'percent') {
+        //console.log(' name=' + UKey + ' value=' + Progress[Pkey][UKey]);
+          porcentajetotal += parseInt(Progress[Pkey][UKey]);
+        }
+        if (UKey === 'units') {
+          for (var uniKey in Progress[Pkey][UKey]) {
+            console.log('\t\tUnidad=' + uniKey );
+            for (var xKey in Progress[Pkey][UKey][uniKey]) {
+              if (xKey === 'parts') {
+                //console.log('\t\t\t\tname=' + xKey + ' value=' + Progress[Pkey][UKey][uniKey][xKey]);
+                let countPart = 0;
+                let totalRead = 0;
+                let totalQuiz = 0;
+                let totalWorkshop = 0;
+                let totalReadOk = 0;
+                let totalQuizOk = 0;
+                let totalWorkshopOK = 0;
+                let totalScoreQuiz = 0;
+                let scoreSumQuiz = 0;
+                let scoreAvgQuiz = 0;
+                for (partKey in Progress[Pkey][UKey][uniKey][xKey]) {
+                  //console.log('\t\t\t\t\t\tname=' + partKey + ' value=' + Progress[Pkey][UKey][uniKey][xKey][partKey]);
+                //console.log('\t\t\t\t\t\t\t\tType=' + Progress[Pkey][UKey][uniKey][xKey][partKey].type)
+                //console.log('\t\t\t\t\t\t\t\tComplete=' + Progress[Pkey][UKey][uniKey][xKey][partKey].completed)
+                  switch (Progress[Pkey][UKey][uniKey][xKey][partKey].type) {
+                  case 'read':
+                    totalRead++;
+                    if (Progress[Pkey][UKey][uniKey][xKey][partKey].completed === 1)
+                      totalReadOk++;
+                    break;
+                  case 'quiz':
+                    totalQuiz++;
+                    if (Progress[Pkey][UKey][uniKey][xKey][partKey].completed === 1) {
+                      totalQuizOk++;						
+                      scoreSumQuiz += parseInt(Progress[Pkey][UKey][uniKey][xKey][partKey].score);
+                    }
+                      
+                    break;
+                  case 'workshop':
+                    totalWorkshop++;
+                    if (Progress[Pkey][UKey][uniKey][xKey][partKey].completed === 1)
+                      totalWorkshopOK++;						
+                    break;
+                  }
+                }
+                console.log('Totales     ' + totalRead + ' reads,' + totalQuiz + ' quiz,' + totalWorkshop + ' workshop.'); 
+                console.log('Completados ' + totalReadOk + ' reads,' + totalQuizOk + ' quiz,' + totalWorkshopOK + ' workshop.'); 
+                console.log('Porcentajes ' + ((totalReadOk * 100) / totalRead) + '% reads,'
+                                      + ((totalQuizOk * 100) / totalQuiz) + '% quiz,' 
+                                  + ((totalWorkshopOK * 100) / totalWorkshop) + '% workshop.'); 
+                console.log('total    score quiz :' + scoreSumQuiz); 
+                console.log('promedio score quiz :' + (scoreSumQuiz / totalQuizOk)); 
+              }  
+            }
+          }
+        }
+      }
+      console.log('***********');
+    }
+    console.log('porcentaje :' + (porcentajetotal / 8) + '%');
+// Termina lógica
+  });
   console.log(users);
   console.log(progress);
   console.log(courses);
